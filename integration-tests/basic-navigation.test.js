fde6c8b1143df7bff9026529919e3a26e1423028
Nighel123 <nickel.paulsen@gmail.com>
2022-02-20 14:02:22 +0100

    ch06 finished

--- integration-tests/basic-navigation.test.js
+++ integration-tests/basic-navigation.test.js
@@ -6,6 +6,34 @@ const app = require('../meadowlark.js')
 let server = null
 let port = null
 
+beforeEach(async() => {
+        port = await portfinder.getPortPromise()
+        server = app.listen(port)
+})
+
+afterEach(() => {
+        server.close()
+})
+
+test('home page links to about page', async () => {
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage()
+        await page.goto(`http://localhost:${port}`)
+        await Promise.all([
+                page.waitForNavigation(),
+                page.click('[data-test-id="about"]'),
+        ])
+        expect(page.url()).toBe(`http://localhost:${port}/about`)
+        await browser.close()
+})
+/*const portfinder = require('portfinder')
+const puppeteer = require('puppeteer')
+
+const app = require('../meadowlark.js')
+
+let server = null
+let port = null
+
 beforeEach(async () => {
   port = await portfinder.getPortPromise()
   server = app.listen(port)
@@ -25,4 +53,4 @@ test('home page links to about page', async () => {
   ])
   expect(page.url()).toBe(`http://localhost:${port}/about`)
   await browser.close()
-})
+})*/
